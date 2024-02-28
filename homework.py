193c995b244418f9c1302e282eb7a88b6a0846c0
Iulianiia <iulianiia.semernykova@gmail.com>
2024-02-28 09:43:10 +0200

    Added a simple CLI calculator in homework.py.

---
+++ homework.py
@@ -0,0 +1,24 @@
+# app.py
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+if __name__ == "__main__":
+    print("Simple CLI Calculator")
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+        exit(1)
+
+    sum_result = add(num1, num2)
+    difference_result = subtract(num1, num2)
+
+    print(f"Addition Result: {num1} + {num2} = {sum_result}")
+    print(f"Subtraction Result: {num1} - {num2} = {difference_result}")
