60f86be397eb952dbd4c53f670e525a5709ff1b7
Iulianiia <iulianiia.semernykova@gmail.com>
2024-02-28 10:28:11 +0200

    Add division operation to the calculator

--- homework.py
+++ homework.py
@@ -8,9 +8,12 @@ def subtract(x, y):
     return x - y
 
 
-def multiplication(x, y):
-    return x * y
-
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Division by zero"
+    
 
 if __name__ == "__main__":
     print("Simple CLI Calculator")
@@ -23,8 +26,8 @@ if __name__ == "__main__":
 
     sum_result = add(num1, num2)
     difference_result = subtract(num1, num2)
-    multiplication_result = multiplication(num1, num2)
+    divide_result = divide(num1, num2)
 
     print(f"Addition Result: {num1} + {num2} = {sum_result}")
     print(f"Subtraction Result: {num1} - {num2} = {difference_result}")
-    print(f"Multiplication Result: {num1} * {num2} = {multiplication_result}")
+    print(f"Division Result: {num1} * {num2} = {divide_result}")
